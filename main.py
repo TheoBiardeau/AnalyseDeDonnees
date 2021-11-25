737c5d868265602c0b58791429f5ab00414fc7a6
TheoBiardeau <theo.biardeau@etu.univ-poitiers.fr>
2021-11-25 08:39:48 +0100

    Add array

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
+X = np.array(([1,2,3]),([4,5,6]))
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
